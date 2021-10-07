6f8717778d5ef5f67c62f300121552fe2b3e3430
Ada0910 <1355948107@qq.com>
2021-10-07 22:09:43 +0800

    新增auth 模块

---
+++ ada-auth-module/ada-auth/src/main/java/com/ada/auth/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.ada.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @Description: Spring Security配置类
+ *
+ * @ClassName : WebSecurityConfig
+ * @date :2021/10/7  16:56
+ * @author : Ada
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * 自定义鉴权规则
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+
+	/**
+	 * 加密方式 security2.0以后 密码无法明文保存，必须要经过加密
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	/**
+	 * 将 AuthenticationManager 注入到spring容器中
+	 *   AuthenticationManager 默认是不对外开放得
+	 */
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+}
