b9dde6f429a026b208741508a0cebeda10365b64
Ada0910 <1355948107@qq.com>
2021-10-26 23:47:05 +0800

    oauth2配置文件

---
+++ ada-auth-module/ada-auth/src/main/java/com/ada/auth/config/Oauth2ServerConfig.java
@@ -0,0 +1,15 @@
+package com.ada.auth.config;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+/**
+ * @Description:
+ *
+ * @ClassName : Oauth2ServerConfig
+ * @date :2021/10/26  23:26
+ * @author : Ada
+ */
+public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
+	//private final PasswordEncoder passwordEncoder;
+}
